edd0cbf6969905050cd75acf71eec544b6cd7e4c
Akhila Pynam <akhilapynam@gmail.com>
2026-07-11 00:48:35 +0530

    done

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    "http://localhost:5000/api",
 });
 
 // Preview CSV
 export const previewCSV = async (file: File) => {
   const formData = new FormData();
-
   formData.append("file", file);
 
   const response = await api.post("/preview", formData, {
@@ -19,10 +20,9 @@ export const previewCSV = async (file: File) => {
   return response.data;
 };
 
-// Import CSV
+// Upload CSV
 export const uploadCSV = async (file: File) => {
   const formData = new FormData();
-
   formData.append("file", file);
 
   const response = await api.post("/upload", formData, {
